e8774f62cfbddf370911dfc6bbcb729f2047bb87
Titome <github@timotheegirard.com>
2015-06-14 21:48:21 +0200

    add tests

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,9 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
-# Create your tests here.
+class HomeViewTests(TestCase):
+    def test_index_view(self):
+        response = self.client.get(reverse('index'))
+        #response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Timothée Girard')
